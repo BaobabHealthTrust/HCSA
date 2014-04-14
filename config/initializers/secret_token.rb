3e5e7dce6068d23d1476c414703e1d5ff5b6e932
chisonjoka <cenjoka@gmail.com>
2014-04-14 11:00:21 +0200

    added project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cvsa::Application.config.secret_token = '6943e642f650223c0bd1776fb7df5ea34490dce0137b4beaaebdfde57ff269bc57ae730316693f7781049b199aeffc4a072def9a8c0a028dc59c0de3fd9f5ca6'
