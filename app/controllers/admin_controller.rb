51f993e75ccf005085e130f17261a87e666bfbd7
sothini <kaongallostam@gmail.com>
2014-05-29 21:29:52 +0200

    working on report

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,7 +12,23 @@ class AdminController < ApplicationController
 	def forth
 		@votes = Vote.all
 		@satlevel = Satlevel.all
-		@concerns = Concern.all
+	 
+
+	
+		@result = Satlevel.joins(:votes)
+		@result2 = @result.find_by_sql("select name, date, count(*) as 'total_votes' from satlevels left join votes on
+ 						satlevels.satlevel_id = votes.satlevel_id group by satlevels.satlevel_id ")
+		
+		
+		@concern = Concern.joins(:vote_concerns)
+		@concern2 = @concern.find_by_sql(" select name, count(*) as 'total' from vote_concerns join concerns on 
+						   concerns.concern_id = vote_concerns.concern_id group by concerns.concern_id")
+
 	end
 
 end
+
+
+
+
+ 
