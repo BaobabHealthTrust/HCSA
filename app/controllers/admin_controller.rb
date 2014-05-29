f2fc2a0465402524ed3330fc33dc86d99003c611
thandiwe92 <silviamandala92@gmail.com>
2014-05-29 21:20:31 +0200

    delete and add concerns

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -20,6 +20,34 @@ class AdminController < ApplicationController
 		@service = Service.all
 	end
 
+	def listconcerns
+		@concerns = Concern.all
+	end
+
+	def addConcerns
+
+	concerns = Concern.new(params[:add_concern])
+    	
+
+  	 	 if concerns.save
+		  flash[:notice] = "Concern added."
+   		   redirect_to :action=>"listconcerns"
+    		end
+
+	
+	end
+
+	def delconcern
+ 	@concern = Concern.find(params[:id]) 
+	end 
+
+	def destroy 
+	Concern.find(params[:id]).destroy 
+	flash[:notice] = "Concern deleted." 
+	redirect_to(:action => 'listconcerns') 
+	end 
+
+
 	def login
 
 	end
